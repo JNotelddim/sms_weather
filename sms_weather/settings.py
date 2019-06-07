09d8e8c744c8404e9d455584c9d2f32ee55da0b6
Jared Middleton <jaredmiddleton3.14@gmail.com>
2019-06-07 15:37:39 -0700

    Another attempt at heroku static files settings

--- sms_weather/settings.py
+++ sms_weather/settings.py
@@ -118,10 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(PROJECT_ROOT, 'static'),
 )
